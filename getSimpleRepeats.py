6f4f6522bf97b433380424d4c28c7044f222fbb2
Goutham <goutham.atla@gmail.com>
2015-11-18 00:01:53 +0100

    Update getSimpleRepeats.py

--- getSimpleRepeats.py
+++ getSimpleRepeats.py
@@ -6,6 +6,7 @@ And if a SNP is present at the end or begening of simple repeat GAAAAAA ot AAAAA
 Requirements: BioPython, bedtools
 
 usage:
+
 python getSimpleRepeats.py in.fasta | bedtools merge > out.bed
 
 or to make it faster, run on individual chromosomes
@@ -14,8 +15,7 @@ parallel "python getSimpleRepeats.py {} | bedtools merge > {/.}.bed " ::: *.fa
 
 '''
 
-import re
-import sys
+import re,sys
 from Bio import SeqIO
 
 def return_simpleRepeats(id,seqence):
@@ -23,6 +23,9 @@ def return_simpleRepeats(id,seqence):
   repeat = ["\t".join((id, str(m.start()),str(m.end()+1))) for m in re.finditer(pattern,seqence)]
   if repeat:
 		for i in repeat:
-			print i	
+			print i
+			
 for seq in SeqIO.parse(sys.argv[1],"fasta"):
 	return_simpleRepeats(str(seq.id),str(seq.seq))
+
+
